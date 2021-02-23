046cfb77985e4e5de3c55cc8cf7b77fb1981ed96
Vitaly Litvinovich <vitaly@wsa-242-73b.local>
2021-02-23 17:45:37 +0300

    feat: add filters to filter screen

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Platform, ScrollView} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {enableScreens} from 'react-native-screens';
 
 import DrawerNavigator from './src/components/navigators/DrawerNavigator';
-
-import COLORS from './src/constants/Colors';
 
 enableScreens();
 
--- src/screens/FiltersScreen.js
+++ src/screens/FiltersScreen.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Switch,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+import DefaultText from '../components/DefaultText';
+import Colors from '../constants/Colors';
+
+const FilterSwitch = ({onValueChange, value, label}) => (
+  <TouchableWithoutFeedback onPress={() => onValueChange(!value)}>
+    <View style={styles.filterContainer}>
+      <Text>{label}</Text>
+      <Switch
+        trackColor={{
+          true: Colors.secondary,
+          false: 'grey',
+        }}
+        thumbColor={'white'}
+        value={value}
+      />
+    </View>
+  </TouchableWithoutFeedback>
+);
 
 const FiltersScreen = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The filters screen!</Text>
+      <DefaultText style={styles.title}>Available Filters</DefaultText>
+      <FilterSwitch
+        label="Gluten free"
+        value={isGlutenFree}
+        onValueChange={setIsGlutenFree}
+      />
+      <FilterSwitch label="Vegan" value={isVegan} onValueChange={setIsVegan} />
+      <FilterSwitch
+        label="Vegetarian"
+        value={isVegetarian}
+        onValueChange={setIsVegetarian}
+      />
+      <FilterSwitch
+        label="Lactose free"
+        value={isLactoseFree}
+        onValueChange={setIsLactoseFree}
+      />
     </View>
   );
 };
@@ -12,8 +59,16 @@ const FiltersScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  filterContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  title: {
+    fontSize: 22,
   },
 });
 
